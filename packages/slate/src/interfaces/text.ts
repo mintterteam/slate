3a966835e5d15dfc446e4e6cad47d302e58067c4
Jonas Kruckenberg <iterpre@protonmail.com>
2021-08-31 11:16:44 +0200

    fix: use correct property for isText test

--- packages/slate/src/interfaces/text.ts
+++ packages/slate/src/interfaces/text.ts
@@ -47,7 +47,7 @@ export const Text: TextInterface = {
    */
 
   isText(value: any): value is Text {
-    return isPlainObject(value) && typeof value.text === 'string'
+    return isPlainObject(value) && typeof value.value === 'string'
   },
 
   /**
